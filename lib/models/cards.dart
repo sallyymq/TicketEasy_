b2834d4b9829cc4be70c7127f43cfad8fc1b6278
Raghad Abo Ajamia <118125828+RaghadAjamia@users.noreply.github.com>
2024-06-13 16:31:28 +0300

    Update cards.dart

--- lib/models/cards.dart
+++ lib/models/cards.dart
@@ -2,14 +2,21 @@ class CCard {
   String Complex;
   String Status;
   String BusNumber;
+  String Bus_Num;
+  int Seats_Num;
 
   CCard({
     required this.Complex,
     required this.Status,
     required this.BusNumber,
+    required this.Bus_Num,
+    required this.Seats_Num,
   });
 
   String get _Complex => Complex;
   String get _Status => Status;
   String get _BusNumber => BusNumber;
+    String get _Bus_Num => Bus_Num;
+  int get _Seats_Num => Seats_Num;
+
 }
